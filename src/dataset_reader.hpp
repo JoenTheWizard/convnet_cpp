b0d45fc2f830e69e84b22974235e3d4d8f5e7a28
U-JOENWIZ-93\Baher <joenthewizard0@gmail.com>
2024-11-20 21:38:50 +1100

    Update read_csv() and read_csv_classification() to not use vector<vector<double>> data which could be a memory overhead to larger datasets

--- src/dataset_reader.hpp
+++ src/dataset_reader.hpp
@@ -37,7 +37,6 @@ public:
 
     //Read CSV file (default delimiter is comma and ignore first row 'header' by default)
     //This will read the data as double types
-    //NOTE: Might need to improve this function by removing use of 'data' vector vector double
     void read_csv(const std::string& filename, bool has_header = true, char delimiter = ',') {
         std::ifstream file(filename);
         if (!file.is_open()) {
@@ -45,96 +44,124 @@ public:
         }
 
         std::string line;
-        std::vector<std::vector<double>> data;
+        int rows = 0, cols = 0;
 
-        //Read header if available
-        if (has_header && std::getline(file, line)) {
+        //= First pass =
+
+        //Read number of columns. Also check for the header if available
+        if (std::getline(file, line)) {
             std::istringstream iss(line);
             std::string token;
             while (std::getline(iss, token, delimiter)) {
-                header.push_back(token);
+                if (has_header) header.push_back(token);
+                cols++;
             }
+            if (!has_header) rows++;
         }
 
-        //Read data
         while (std::getline(file, line)) {
-            std::istringstream iss(line);
-            std::string token;
-            std::vector<double> row;
-            while (std::getline(iss, token, delimiter)) {
-                row.push_back(std::stod(token));
-            }
-            data.push_back(row);
+            rows++;
         }
 
-        int rows = data.size();
-        int cols = data[0].size();
-
+        //Initialize matrices
         features = Matrix(rows, cols - num_outputs);
         labels   = Matrix(rows, num_outputs);
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols - num_outputs; j++) {
-                features(i, j) = data[i][j];
+        //Reset file stream to read data again for second pass
+        file.clear();
+        file.seekg(0);
+
+        //= Second pass =
+
+        //Skip header if any
+        if (has_header && std::getline(file, line)) {}
+
+        //Read data
+        int current_row = 0;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            std::string token;
+            int current_col = 0;
+
+            while (std::getline(iss, token, delimiter)) {
+                if (current_col < cols - num_outputs) {
+                    features(current_row, current_col) = std::stod(token);
+                }
+                else {
+                    labels(current_row, current_col - (cols - num_outputs)) = std::stod(token);
+                }
+                current_col++;
             }
-            for (int j = 0; j < num_outputs; j++) {
-                labels(i, j) = data[i][cols - num_outputs + j];
-            }
+            current_row++;
         }
     }
 
-    //Read CSV file for classification. Will map outputs accordingly to the label matrix
+    //Read CSV file for classification. Will map outputs accordingly to the label matrix with one-hot encoding
     //Ensure that the label_map input is in the same orientation for the output model
-    //NOTE: Might need to improve this function by removing use of 'data' vector vector string
-    void read_csv_classification(const std::string& filename, std::vector<std::string> label_map, bool has_header = true, char delimiter = ',') {
+    void read_csv_classification(const std::string& filename, const std::vector<std::string>& label_map, bool has_header = true, char delimiter = ',') {
         std::ifstream file(filename);
         if (!file.is_open()) {
             throw std::runtime_error("[-] ERROR Dataset_reader.cpp: Unable to open file: " + filename);
         }
 
         std::string line;
-        std::vector<std::vector<std::string>> data;
+        int rows = 0, cols = 0;
 
-        //Read header if available
-        if (has_header && std::getline(file, line)) {
+        //= First pass =
+
+        //Read number of columns. Also check for the header if available
+        if (std::getline(file, line)) {
             std::istringstream iss(line);
             std::string token;
             while (std::getline(iss, token, delimiter)) {
-                header.push_back(token);
+                if (has_header) header.push_back(token);
+                cols++;
             }
+            if (!has_header) rows++;
         }
 
-        //Read data
         while (std::getline(file, line)) {
-            std::istringstream iss(line);
-            std::string token;
-            std::vector<std::string> row;
-            while (std::getline(iss, token, delimiter)) {
-                row.push_back(token);
-            }
-            data.push_back(row);
+            rows++;
         }
-
-        int rows = data.size();
-        int cols = data[0].size();
 
         size_t label_size = label_map.size();
 
         //Set num_outputs to the number of classes
         this->num_outputs = label_size;
         
+        //Initialize matrices
         features = Matrix(rows, cols - 1);
         labels   = Matrix(rows, label_size);
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols - 1; j++) {
-                features(i, j) = std::stod(data[i][j]);
-            }
-            for (int j = 0; j < label_size; j++) {
-                if (label_map[j] == data[i][cols - 1]) {
-                    labels(i, j) = 1;
+        //Reset file stream to read data again for second pass
+        file.clear();
+        file.seekg(0);
+
+        //= Second pass =
+
+        //Skip header if any
+        if (has_header && std::getline(file, line)) {}
+
+        int current_row = 0;
+        while (std::getline(file, line)) {
+            std::istringstream iss(line);
+            std::string token;
+            int current_col = 0;
+
+            while (std::getline(iss, token, delimiter)) {
+                if (current_col < cols - 1) {
+                    features(current_row, current_col) = std::stod(token);
                 }
+                else {
+                    for (size_t j = 0; j < label_size; j++) {
+                        if (label_map[j] == token) {
+                            labels(current_row, j) = 1;
+                        }
+                    }
+                }
+                current_col++;
             }
+            current_row++;
         }
     }
 
