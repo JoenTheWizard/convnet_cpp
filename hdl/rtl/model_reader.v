b0c58bf88de19ca5d1f498afe53cf75e09c5238d
U-JOENWIZ-93\Baher <joenthewizard0@gmail.com>
2024-12-29 00:05:35 +1100

    Update model_reader module to read quantized models, will need a Verilog header to set the appropriate headers during compile time

--- hdl/rtl/model_reader.v
+++ hdl/rtl/model_reader.v
@@ -10,30 +10,59 @@ module model_reader #(
     output reg read_done
 );
 
-integer file_handle, bytes_read;
+integer file_handle, bytes_read, offset;
 reg [WEIGHT_WIDTH-1:0] data [0:1023];
 reg [31:0] num_layers, num_rows, num_cols;
+reg [7:0] max_weight, activation;
 
-//This module is just for reading the binary file and loading it into registers
+//This module is just for reading the quantized model's binary file and printing it
+//NOTE: The this reads the models quantized to 8-bit precision
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
         read_done <= 0;
-    end 
+    end
     else if (start_read) begin
-        file_handle = $fopen("../mnist_quantized.weights", "rb");
+        file_handle = $fopen("../xor_q2.weights", "rb");
         if (file_handle == 0) begin
             $display("[-] ERROR: Failed to open file");
-        end 
+        end
         else begin
             bytes_read = $fread(data, file_handle);
 
+            //Header
             num_layers = {data[3], data[2], data[1], data[0]};
-            num_rows   = {data[7], data[6], data[5], data[4]};
-            num_cols   = {data[11], data[10], data[9], data[8]};
+            max_weight = {data[4]};
             $display("Number of layers: %0d", num_layers);
-            $display("Number of rows: %0d", num_rows);
-            $display("Number of columns: %0d", num_cols);
+            $display("Number of signed bits integer occupies: %0d", max_weight);
+
+            //Byte offset from header
+            offset = 5;
+
+            $display("======== WEIGHTS =========");
+
+            for (integer i = 0; i < num_layers; i++) begin
+                num_rows = {data[offset+3], data[offset+2], data[offset+1], data[offset]};
+                offset = offset + 4;
+                num_cols = {data[offset+3], data[offset+2], data[offset+1], data[offset]};
+                offset = offset + 4;
+
+                $display("Layer %0d:", i+1);
+                $display("Number of rows: %0d", num_rows);
+                $display("Number of columns: %0d", num_cols);
+
+                for (integer j = 0; j < num_rows; j++) begin
+                    for (integer k = 0; k < num_cols; k++) begin
+                        $write("%0d ", $signed(data[offset]));
+                        offset = offset + 1;
+                    end
+                    $display("");
+                end
+
+                activation = data[offset];
+                $display("Activation function: %0d", activation);
+                offset = offset + 1;
+            end
             
             $fclose(file_handle);
             read_done <= 1;
