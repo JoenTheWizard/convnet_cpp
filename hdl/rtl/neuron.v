7e48241fdb6d2ea8f5b5654c7e29f70972657dbd
joenwiz <joenthewizard0@gmail.com>
2025-01-23 21:16:49 +1100

    Pass in the activation look-up table in the neuron module after accumulation, might try to simplify the state machine

--- hdl/rtl/neuron.v
+++ hdl/rtl/neuron.v
@@ -1,6 +1,7 @@
 module neuron #(
-    parameter SIZE  = 2,  //Number of elements in each array
-    parameter WIDTH = 8   //Bit width of each element
+    parameter SIZE     = 2,                       //Number of elements in each array
+    parameter WIDTH    = 8,                       //Bit width of each element
+    parameter LUT_FILE = "build/sigmoid_lut.mem"  //Set the activation LUT file
 )(
     input wire clk,
     input wire reset,
@@ -18,8 +19,21 @@ module neuron #(
 reg [1:0] state;
 reg [$clog2(SIZE):0] counter;
 reg signed [2*WIDTH:0] accumulator;
+wire [WIDTH-1:0] activation_result;
 
-localparam IDLE = 2'b00, MULTIPLY = 2'b01, FINISH = 2'b10;
+localparam IDLE = 2'b00, MULTIPLY = 2'b01, ACTIVATE = 2'b10, FINISH = 2'b11;
+
+//Pass the accumulated values (inputs & weights) to the LUT, the result will
+//be latched to the activation_result at the FINISH state
+activation_lut #(
+    .LUT_FILE(LUT_FILE),
+    .ADDR_WIDTH(WIDTH),
+    .DATA_WIDTH(WIDTH)
+) act_lut (
+    .clk(clk),
+    .address(accumulator[WIDTH-1:0]),
+    .data_out(activation_result)
+);
 
 //This module is for a singular sequential neuron cell that multiplies SIZE x SIZE set and appends the result through the accumulator
 
@@ -49,12 +63,16 @@ always @(posedge clk or posedge reset) begin
                 counter <= counter + 1;
 
                 if (counter == SIZE - 1) begin
-                    state <= FINISH;
+                    state <= ACTIVATE;
                 end
             end
 
+            ACTIVATE: begin
+                state <= FINISH;
+            end
+
             FINISH: begin
-                result <= accumulator;
+                result <= activation_result;
                 valid  <= 1;
                 done   <= 1;
                 state  <= IDLE;
